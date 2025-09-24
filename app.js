c6c2881d770644cc4228984333ae415809194482
Varad Jadhav <varad1708.vj@gmail.com>
2025-09-24 10:37:57 +0530

    UI UX changed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 window.showMessage = function() {
     const message = document.getElementById("message");
-    message.textContent = "Please Login to make your first invoice";
+    message.textContent = "Please Signup to make your first invoice";
 };
