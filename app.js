a50ddb631018af298286e099326bc4096083e2ad
Varad Jadhav <varad1708.vj@gmail.com>
2025-09-24 21:24:11 +0530

    Exp 6 (SIGNUP PAGE)

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-window.showMessage = function() {
+function showMessage() {
     const message = document.getElementById("message");
     message.textContent = "Please Signup to make your first invoice";
 };
+
+
+function validateForm() { 
+  const emailInput = document.getElementById("email"); 
+  const email = emailInput.value.trim(); 
+  const phoneInput = document.getElementById("phoneNumber");
+  const phone = phoneInput.value.trim();
+
+  if (email.indexOf("@") === -1 || email.indexOf(".") === -1) { 
+    alert("Invalid email address. Please include '@' and '.' characters."); 
+    emailInput.focus(); 
+    return false; 
+  }
+
+  if (phone.length !== 10) {
+    alert("Phone number is not valid. It must be exactly 10 digits.");
+    phoneInput.focus();
+    return false;
+  }
+
+  alert("Email is valid and phone number is valid!");
+  return true;
+}
